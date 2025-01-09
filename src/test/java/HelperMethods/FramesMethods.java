8e97ce47e4539c903fa489dba03975e7970fe2ac
alex <tomusalex@gmail.com>
2025-01-09 16:24:50 +0200

    Refactor Helper

--- src/test/java/HelperMethods/FramesMethods.java
+++ src/test/java/HelperMethods/FramesMethods.java
@@ -1,4 +1,22 @@
 package HelperMethods;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class FramesMethods {
+    WebDriver driver;
+
+    public FramesMethods(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void switchToFrames(WebElement element){
+        // schimba focusul de pe un frame anume
+        driver.switchTo().defaultContent();
+    }
+
+    public void switchToMainContent(){
+        // schimbam focusul pe pagina principala
+        driver.switchTo().defaultContent();
+    }
 }
--- src/test/java/HelperMethods/WindowsMethods.java
+++ src/test/java/HelperMethods/WindowsMethods.java
@@ -1,4 +1,50 @@
 package HelperMethods;
 
+import org.openqa.selenium.WebDriver;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class WindowsMethods {
+    WebDriver driver;
+
+    public WindowsMethods(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void switchToOpenedTab(){
+        // driver.getWindowHandles() -> returneaza toate windowsurile deschise
+        List<String> tabList = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(tabList.get(1));
+    }
+
+    public void switchToMainTab(){
+        switchToMain();
+    }
+
+    public void switchToOpenedWindows(){
+        switchToOpenedTab();
+    }
+
+    private void close(){
+        driver.close();
+    }
+
+    public void closeTab(){
+        close();
+    }
+
+    public void closeWindows(){
+        close();
+    }
+    public void switchToMainWindows(){
+        switchToMain();
+    }
+
+    public void switchToMain(){
+        // driver.getWindowHandles() -> returneaza toate windowsurile deschise
+        List<String> tabList = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(tabList.get(0));
+    }
+
 }
